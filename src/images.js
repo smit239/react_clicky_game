b3c8774729d82e515694d95204eb6a9e58697f3f
Rodney Smith <smit239@gmail.com>
2019-09-23 18:52:14 -0400

    Marvel images.js created

---
+++ src/images.js
@@ -0,0 +1,34 @@
+import bathman1 from './images/batman_1.jpg';
+import blackpanther1 from './images/blackpanther_1.jpg';
+import captain_america1 from './images/captain_america1.jpg';
+import captain_america2 from './images/captain_america2.jpg';
+import captain_america3 from './images/captain_america3.png';
+import captain_america4 from './images/captain_america4.jpg'; 
+import hulk from './images/hulk.jpg';
+import ironman_1 from './images/ironman_1.png';
+import ironman_2 from './images/ironman_2.jpg';
+import ironman_3 from './images/ironman_3.jpg';
+import ironman_4 from './images/ironman_4.jpg';
+import spiderman1 from './images/spiderman_1.jpg';
+import spiderman2 from './images/spiderman2.jpg';
+import thore_1 from './images/thore_1.jpg'; 
+
+
+const images = [
+    batman1,
+    blackpanther1,
+    captain_america1,
+    captain_america2,
+    captain_america3,
+    captain_america4,
+    hulk_1,
+    ironman_1,
+    ironman_2,
+    ironman_3,
+    ironman_4,
+    spiderman_1,
+    spiderman_2,
+    thore_1
+];
+
+export default images;

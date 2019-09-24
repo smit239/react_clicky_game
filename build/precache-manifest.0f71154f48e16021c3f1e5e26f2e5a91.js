bbf1b68d8aaa8dff126984307861623b677a32da
Rodney Smith <smit239@gmail.com>
2019-09-23 21:58:11 -0400

    npm run build

---
+++ build/precache-manifest.0f71154f48e16021c3f1e5e26f2e5a91.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3aa0a36d91ff82242392abcd71a1b4",
+    "url": "/smit239/react_clicky_game/index.html"
+  },
+  {
+    "revision": "d8f79550552495dcc719",
+    "url": "/smit239/react_clicky_game/static/css/main.029ea7f5.chunk.css"
+  },
+  {
+    "revision": "440da50241becfbfcae8",
+    "url": "/smit239/react_clicky_game/static/js/2.f5f501f7.chunk.js"
+  },
+  {
+    "revision": "d8f79550552495dcc719",
+    "url": "/smit239/react_clicky_game/static/js/main.e0888fd7.chunk.js"
+  },
+  {
+    "revision": "b438775f7996df1d76ae",
+    "url": "/smit239/react_clicky_game/static/js/runtime-main.44da96e9.js"
+  },
+  {
+    "revision": "481d24e54dac147564c817fe0b11e814",
+    "url": "/smit239/react_clicky_game/static/media/Marvel_All.481d24e5.jpg"
+  },
+  {
+    "revision": "3abb3ad474bf4dbb34020fb873214713",
+    "url": "/smit239/react_clicky_game/static/media/batman_1.3abb3ad4.jpg"
+  },
+  {
+    "revision": "830795dbcb6ab4d01d10d866bf15ee12",
+    "url": "/smit239/react_clicky_game/static/media/blackpanther_1.830795db.jpg"
+  },
+  {
+    "revision": "488da7fcaa592d6bc7ba2dd35085a74e",
+    "url": "/smit239/react_clicky_game/static/media/captain_america1.488da7fc.jpg"
+  },
+  {
+    "revision": "6d54c873bf754b55d8240e46b7923c63",
+    "url": "/smit239/react_clicky_game/static/media/captain_america2.6d54c873.jpg"
+  },
+  {
+    "revision": "e2907ebbd316bf15bb1c133778f7d521",
+    "url": "/smit239/react_clicky_game/static/media/captain_america3.e2907ebb.png"
+  },
+  {
+    "revision": "f3ab549a09946c969211009d95680a1a",
+    "url": "/smit239/react_clicky_game/static/media/captain_america4.f3ab549a.jpg"
+  },
+  {
+    "revision": "a543ce959f5ab9b390bf31a2e4bd809a",
+    "url": "/smit239/react_clicky_game/static/media/hulk_1.a543ce95.jpg"
+  },
+  {
+    "revision": "7500594d61ee864209eb9a334b8eb5c0",
+    "url": "/smit239/react_clicky_game/static/media/ironman_1.7500594d.png"
+  },
+  {
+    "revision": "549819ce3675fd457c12cc083d705ab4",
+    "url": "/smit239/react_clicky_game/static/media/ironman_2.549819ce.jpg"
+  },
+  {
+    "revision": "a187d551b6496d8a29e223a683ecdec7",
+    "url": "/smit239/react_clicky_game/static/media/ironman_3.a187d551.jpg"
+  },
+  {
+    "revision": "77752f34fe8be5758ed41052b91d1668",
+    "url": "/smit239/react_clicky_game/static/media/ironman_4.77752f34.jpg"
+  },
+  {
+    "revision": "0384648ba1a80fb4ef59b602d495db8c",
+    "url": "/smit239/react_clicky_game/static/media/spiderman_1.0384648b.jpg"
+  },
+  {
+    "revision": "1171630c78a9c1fda376f819bdbe651c",
+    "url": "/smit239/react_clicky_game/static/media/spiderman_2.1171630c.jpg"
+  },
+  {
+    "revision": "cf4cc84516528e6b6e6a69e4f2cac415",
+    "url": "/smit239/react_clicky_game/static/media/thore_1.cf4cc845.jpg"
+  }
+]);
--- src/images.js
+++ src/images.js
@@ -1,22 +1,22 @@
-import bathman1 from './images/batman_1.jpg';
-import blackpanther1 from './images/blackpanther_1.jpg';
+import batman_1 from './images/batman_1.jpg';
+import blackpanther_1 from './images/blackpanther_1.jpg';
 import captain_america1 from './images/captain_america1.jpg';
 import captain_america2 from './images/captain_america2.jpg';
 import captain_america3 from './images/captain_america3.png';
 import captain_america4 from './images/captain_america4.jpg'; 
-import hulk from './images/hulk.jpg';
+import hulk_1 from './images/hulk_1.jpg';
 import ironman_1 from './images/ironman_1.png';
 import ironman_2 from './images/ironman_2.jpg';
 import ironman_3 from './images/ironman_3.jpg';
 import ironman_4 from './images/ironman_4.jpg';
-import spiderman1 from './images/spiderman_1.jpg';
-import spiderman2 from './images/spiderman2.jpg';
+import spiderman_1 from './images/spiderman_1.jpg';
+import spiderman_2 from './images/spiderman_2.jpg';
 import thore_1 from './images/thore_1.jpg'; 
 
 
 const images = [
-    batman1,
-    blackpanther1,
+    batman_1,
+    blackpanther_1,
     captain_america1,
     captain_america2,
     captain_america3,

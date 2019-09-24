4f6f17e04e390065064fa0372197748a43fa94df
Rodney Smith <smit239@gmail.com>
2019-09-23 22:17:11 -0400

    update npm run build

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => <div className="footer text-center">Trevor Johnson</div>;
+const Footer = () => <div className="footer text-center">Rodney Smith</div>;
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickyGame from './src/components/ClickyGame';
+import ClickyGame from './components/ClickyGame';
 import './index.css';
 
 ReactDOM.render(<ClickyGame />, document.getElementById('root'));

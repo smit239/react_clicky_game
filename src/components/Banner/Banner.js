458fa8c46405c9d6f23046b726be07c2c35dfb6b
Rodney Smith <smit239@gmail.com>
2019-09-23 22:30:54 -0400

    font adjusted on banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => (
+  
   <div className="banner text-center d-flex align-items-center justify-content-center">
-    <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
+    <p className="m-0">Click on an image below to earn points, but don't click on the same image more than once!</p>
   </div>
 );
 
